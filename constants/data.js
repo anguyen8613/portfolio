860ff7987efc44d89fa5c0a7d25d194f2f3c8bc5
unknown <anguyen@softechiq.com>
2021-10-08 16:11:16 -0700

    Add portfolio

---
+++ constants/data.js
@@ -0,0 +1,76 @@
+const userData = {
+  githubUsername: "anguyen8613",
+  name: "Alvin Nguyen",
+  designation: "Full-Stack Developer",
+  avatarUrl: "/avatar2.png",
+  email: "anguyen8613@gmail.com",
+  phone: "657-334-9818",
+  address: "Stanton, California, USA",
+  projects: [
+    {
+      title: "Fire-Movies",
+      link: "https://fire-movies.vercel.app/",
+      imgUrl: "/fire-movies.png"
+    },
+    {
+      title: "Rendezvous",
+      link: "https://rendezvous-kohl.vercel.app/",
+      imgUrl: "/rendezvous.png"
+    },
+    {
+      title: "Pets-Social",
+      link: "https://social-pet-friends.netlify.app/",
+      imgUrl: "/pets-social.png"
+    }
+  ],
+  about: {
+    title:
+      "I'm a software developer that loves building products and web applications that impact people and businesses",
+    description: [
+      `I've been developing full-stack applications for 3 years.  I started with learning Javascript and Java from school and went on to teach myself the most innovative technologies.  I introduced these to my current work place and am now 
+        developing using NodeJs, React, Next.js, Typescript, Express, Mongo Atlas.  I also have experience with machine learning with Python SciKit-Learn.`
+    ],
+    currentProject: "Fire Movies",
+    currentProjectUrl: "https://fire-movies.vercel.app/"
+  },
+  experience: [
+    {
+      title: "Software Developer",
+      company: "Soctech & Associates",
+      year: "2021",
+      companyLink: "https://www.softechis.com/",
+      desc:
+        "Contributed to our flagship client - Synchrony Bank - by creating immersive admin applications for the marketing team."
+    },
+    {
+      title: "Internship",
+      company: "Soctech & Associates",
+      year: "2018",
+      companyLink: "https://www.softechis.com/",
+      desc:
+        "Internship for 3 months before hired on full time.  Contributed to documentations and deployment"
+    },
+    {
+      title: "Graduation",
+      company: "California State University Los Angeles",
+      year: "2018",
+      companyLink: "https://www.calstatela.edu/",
+      desc: "Major in Computer Science"
+    },
+    {
+      title: "High School",
+      company: "Westminster High SChool.",
+      year: "2007",
+      companyLink: "https://www.whslions.net/",
+      desc: "Honors, AP Classes"
+    }
+  ],
+  resumeUrl:
+    "https://drive.google.com/file/d/1s88reEQz9sqAK7tAoVmJfN8Bv-4j5trU/view",
+  socialLinks: {
+    linkedin: "https://www.linkedin.com/in/anguyen8613/",
+    github: "https://github.com/anguyen8613"
+  }
+};
+
+export default userData;
