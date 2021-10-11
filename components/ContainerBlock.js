092ecb9fd5ce02da576d938f99303af9567f2f9c
unknown <anguyen@softechiq.com>
2021-10-10 20:48:20 -0700

    Add new avatar

--- components/ContainerBlock.js
+++ components/ContainerBlock.js
@@ -1,48 +1,38 @@
-import React from "react";
-import Head from "next/head";
-import { useRouter } from "next/router";
-import Navbar from "./Navbar";
-import Footer from "./Footer";
+import React from 'react';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+import Navbar from './Navbar';
+import Footer from './Footer';
 
 export default function ContainerBlock({ children, ...customMeta }) {
   const router = useRouter();
 
   const meta = {
-    title: "Alvin Nguyen ",
+    title: 'Alvin Nguyen ',
     description: `I've been developing websites for 3 years straight. Get in touch with me to know more.`,
-    image: "/avatar.png",
-    type: "website",
-    ...customMeta
+    image: '/static/avatar2.png',
+    type: 'website',
+    ...customMeta,
   };
   return (
     <div>
       <Head>
         <title>{meta.title}</title>
-        <meta name="robots" content="follow, index" />
-        <meta content={meta.description} name="description" />
+        <meta name='robots' content='follow, index' />
+        <meta content={meta.description} name='description' />
         <meta
-          property="og:url"
-          content={`https://yourwebsite.com${router.asPath}`}
+          property='og:url'
+          content={`https://portfolio-alvin-nguyen.vercel.app${router.asPath}`}
         />
         <link
-          rel="canonical"
-          href={`https://yourwebsite.com${router.asPath}`}
+          rel='canonical'
+          href={`https://portfolio-alvin-nguyen.vercel.app${router.asPath}`}
         />
-        <meta property="og:type" content={meta.type} />
-        <meta property="og:site_name" content="Alvin Nguyen" />
-        <meta property="og:description" content={meta.description} />
-        <meta property="og:title" content={meta.title} />
-        <meta property="og:image" content={meta.image} />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@mannupaaji" />
-        <meta name="twitter:title" content={meta.title} />
-        <meta name="twitter:description" content={meta.description} />
-        <meta name="twitter:image" content={meta.image} />
         {meta.date && (
-          <meta property="article:published_time" content={meta.date} />
+          <meta property='article:published_time' content={meta.date} />
         )}
       </Head>
-      <main className="dark:bg-gray-800 w-full">
+      <main className='dark:bg-gray-800 w-full'>
         <Navbar />
         <div>{children}</div>
         <Footer />
